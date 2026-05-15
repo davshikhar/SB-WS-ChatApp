0178f02b7d0f25e3565822cc31ed6ccf1f0d39bc
davshikhar <shikharyadav900@gmail.com>
2026-05-15 22:54:28 +0530

    Modified controller and added index.html

--- src/main/java/org/chat/wschat/controller/MainController.java
+++ src/main/java/org/chat/wschat/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
@@ -13,11 +14,19 @@ public class MainController {
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
     public Chat sendMessage(@Payload Chat message){
+        System.out.println(">> Broadcasting: " + message.getContent());
         return Chat.ofChat(message.getSender(), message.getContent());
     }
 
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
     public Chat addUser(@Payload Chat message, SimpMessageHeaderAccessor headerAccessor){
         headerAccessor.getSessionAttributes().put("username",message.getSender());
         return Chat.ofJoin(message.getSender());
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
